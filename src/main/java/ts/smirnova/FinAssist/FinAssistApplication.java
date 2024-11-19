4ecc224ec57ba0838c66c295c9ab9fd42e3ae0dc
tkoptyaeva <av.bardashov@tensor.ru>
2024-11-19 10:21:49 +0300

    Создан проект через генератор spring https://start.spring.io/. Добавлена тестовая начальная страница

---
+++ src/main/java/ts/smirnova/FinAssist/FinAssistApplication.java
@@ -0,0 +1,13 @@
+package ts.smirnova.FinAssist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ts/smirnova/FinAssist/FinAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package ts.smirnova.FinAssist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
